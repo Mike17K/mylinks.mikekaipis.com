0c753217a89077c7ce5544a42ddeb3458d1869e7
Mike Kaipis <kaipismike1@gmail.com>
2025-01-19 20:25:58 +0200

    update

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
-import viteCompression from 'vite-plugin-compression';
-export default defineConfig({
-    plugins: [mkcert(), viteCompression()],
-    server: {
-        port: 4355,
-    },
-});
