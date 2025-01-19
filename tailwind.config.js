5f5c5a5e0f852d63aaf78dc46a27d71b8b4df852
Mike Kaipis <kaipismike1@gmail.com>
2025-01-19 20:10:42 +0200

    build issues fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     './index.html',                  // For plain HTML files
     './src/**/*.{js,jsx,ts,tsx}',     // For React, Vue, or other JS/TS files
